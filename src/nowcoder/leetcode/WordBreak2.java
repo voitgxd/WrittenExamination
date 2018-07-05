da3108907f780d93315d5bccc4b9dc2a61a143c7
voitgxd <1316110738@qq.com>
2018-07-05 23:10:22 +0800

    WordBreak2 commit

---
+++ src/nowcoder/leetcode/WordBreak2.java
@@ -0,0 +1,20 @@
+package nowcoder.leetcode;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+public class WordBreak2 {
+
+    public ArrayList<String> wordBreak(String s, Set<String> dict) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        //s ="catsanddog",
+        //dict =["cat", "cats", "and", "sand", "dog"].
+        //A solution is["cats and dog", "cat sand dog"].
+        String s = "catsanddog";
+        String[] dict = {"cat", "cats", "and", "sand", "dog"};
+        String[] result;
+    }
+}
